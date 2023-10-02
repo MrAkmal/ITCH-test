7405298668eb41e00d6b8f01dfa14129336d08cd
Mingyasharov Akmal <akmal.mingyasharov@dohatec.net>
2023-10-02 11:09:43 +0500

    inputstream error

--- src/main/java/com/example/itchtestservice/soupbintcp/SoupBinTcpReceiver.java
+++ src/main/java/com/example/itchtestservice/soupbintcp/SoupBinTcpReceiver.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class SoupBinTcpReceiver {
@@ -19,22 +21,45 @@ public class SoupBinTcpReceiver {
 
     public String consumeMessage() throws IOException {
 
-        Socket socket = new Socket(serverHost,serverPort);
+        Socket socket = new Socket(serverHost, serverPort);
 
         InputStream inputStream = socket.getInputStream();
 
+        System.out.println("socket.isConnected() = " + socket.isConnected());
+
+//        System.out.println("inputStream = " + inputStream);
+//
 //        int read = inputStream.read();
 //        System.out.println("read = " + read);
 //        byte[] payload = new byte[read];
 //        System.out.println("payload = " + new String(payload));
 
+//        int i = 0;
+//
+//        while ((i = inputStream.read()) != -1) {
+//
+//            // converts integer to character
+//            char c = (char) i;
+//
+//            // prints character
+//            System.out.print("c  = " + c);
+//        }
+
+
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        int character = reader.read();
+        System.out.println("reader.getEncoding() = " + reader.getEncoding());
+        System.out.println("character = " + character);
+
         byte[] payloadBytes = inputStream.readAllBytes();
 
-        String fileName = new String(payloadBytes);
+        System.out.println("payloadBytes = " + payloadBytes);
+
+        String fileName = new String(payloadBytes, StandardCharsets.UTF_8);
 
         System.out.println("fileName = " + fileName);
 
-        fileName = "C:\\Users\\MrAkmal\\Desktop\\Dohasec\\01302019.NASDAQ_ITCH50";
+//        fileName = "C:\\Users\\MrAkmal\\Desktop\\Dohasec\\01302019.NASDAQ_ITCH50";
 
         Parse parse = new Parse(fileName, "C:\\Users\\MrAkmal\\Desktop\\Dohasec\\itch-test-service\\src\\main\\resources\\itch5.yaml");
 
